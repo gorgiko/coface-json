a6317a52814c497bf0e3040b5b3d69ab0880fe30
marijakitanska <marija.kitanska@mbdp.com.mk>
2025-11-15 08:49:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ allowed_fields = {
     "(AOP019)Other tangible assets": ["Other tangible assets","Other tangible fixed assets", "Other equipment" ,"Other Immovables,plants and equipment"],
     "(AOP020)Investments in real estate": ["Investments in real estate"],
     "(AOP021)Financial fixed assets": ["Financial fixed assets", "Long term investments","Long term financial investments and long term receivables Financial fixed assets"],
-    "(AOP031)Long-term receivables": ["Long-term receivables"],
+    "(AOP031)Long-term receivables": ["Long term receivables", "Other long term investments and receivables"],
     "(AOP035)Deferred tax assets": ["DEFERRED TAX ASSETS", "Deferred tax assets"],
-    "(AOP036)Short-term assets": ["Short term assets"],
-    "(AOP037)Inventory": ["Inventory"],
+    "(AOP036)Short-term assets": ["Short term assets", "Current assets", "Total Current assets", "Short term assets"],
+    "(AOP037)Inventory": ["Inventory", "Total inventories", "Inventories"],
     "(AOP045)Short-term receivables": ["Short-term receivables", "SHORT-TERM RECEIVABLES"],
     "(AOP046)Short-term intercompany receivables": ["Short-term intercompany receivables"],
     "(AOP047)Short-term trade receivables": ["Short-term trade receivables"],
@@ -176,6 +176,7 @@ if uploaded_file:
 
     except Exception as e:
         st.error(f"Error: {e}")
+
 
 
 
