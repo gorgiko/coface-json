ffcf332c06a5f2b7db0695c810fb4f0920be641c
gorgiko <122184841+gorgiko@users.noreply.github.com>
2025-11-17 15:00:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from openpyxl.styles import Font, Border, Side
 allowed_fields = {
     "(AOP001)Fixed assets": ["Fixed assets"],
     "(AOP002)Intangible assets": ["I. Intangible assets", "Intangible assets", "Intangible fixed assets", "Total Intangible assets"],
-    "(AOP004)Concessions,patents,licenses and similar rights and other intangible assets":["Concessions,industrial rights,licences","Concessions,patents,licences,trademarks", "Service marks,software and other intangible assets","Concessions,patents,licenses and similar rights and other intangible assets"],
+    "(AOP004)Concessions,patents,licenses and similar rights and other intangible assets":["Concessions,industrial rights,licences","Concessions,patents,licences,trade marks", "Service marks,software and other intangible assets","Concessions,patents,licenses and similar rights and other intangible assets"],
     "(AOP005)Goodwill":["Goodwill"],
     "(AOP006)Advances for intangible assets and intangible assets in preparation":["Advances for intangible assets and intangible assets in preparation"],
     "(AOP008)Other intangible fixed assets":["Other intangible fixed assets"],
@@ -212,6 +212,7 @@ if uploaded_file:
 
     except Exception as e:
         st.error(f"Error: {e}")
+
 
 
 
