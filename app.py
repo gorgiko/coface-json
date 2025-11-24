e62b96864b305d07ddced9ca9fd3a41965e41212
gorgiko <122184841+gorgiko@users.noreply.github.com>
2025-11-24 13:34:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ allowed_fields = {
     "(AOP222)Other operating expenses": ["Other operating expenses","Other expenses","Intangible costs"],
     "(AOP223)Income from financial transactions": ["Income from financial transactions (financial income)","III. FINANCIAL INCOME"],
     "(AOP234)Financial costs": ["Financial costs"],
-    "(AOP250+/AOP251-)Profit or loss before taxation": ["Profit or loss before taxation","Profit before taxation","Loss before taxation"],
+    "(AOP250+/AOP251-)Profit or loss before taxation": ["Profit or loss before taxation","Profit before taxation","Loss before taxation","Profit before taxation/Loss before taxation"],
     "(AOP252)Profit tax": ["Profit tax","Income tax"],
     "(AOP255+/AOP256-)Profit or loss after taxation": ["Profit or loss after taxation","Profit after taxation","Loss after taxation","TOTAL RESULT"],
 }
@@ -254,6 +254,7 @@ if uploaded_file:
 
     except Exception as e:
         st.error(f"Error: {e}")
+
 
 
 
