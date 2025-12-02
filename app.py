ec4b69abe1382f416a7c4a2f9703063ecd3b0455
marijakitanska <marija.kitanska@mbdp.com.mk>
2025-12-02 20:58:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -119,8 +119,8 @@ allowed_fields_netherlands = {
     "(AOP065)Equity capital": ["Equity capital"],
     "(AOP071)Revenue reserves": ["Revenue Reserves"],
     "(AOP075)Profit or loss carried forward": ["Profit or loss carried forward","Retained profit (earnings) of the year (net profits)","Accumulated retained earnings from previous periods","Retained profit", "Retained earnings brought forward", "Net retained profit/net acumalted loss", "Net profit or loss from previos years","Equity, net retained profits/net accumulated losses (balance sheet)","Net profit or loss from previous periods","Accumulated profit reserve","Retained earnings or loss"],
-    "(AOP081)Liabilities": ["Liabilities", "Total liabilities","Total debt","Total debts"],
-    "(AOP085)Long-term liabilities": ["Long-term liabilities", "Long term liabilities", "Total Long term liabilities","Long-term financial and operating liabilities","Long-term provisions and long-term liabilities"],
+    "(AOP081)Total Liabilities": ["Total liabilities"],
+    "(AOP085)Total Long term liabilities": ["Total Long term liabilities"],
     "(AOP093)Other long-term liabilities": ["Other long-term liabilities", "Other long term liabilities"],
     "(AOP095)Short-term liabilities": ["Short-term liabilities","IV. SHORT-TERM LIABILITIES","Short- term liabilities","Short term liabilities","SHORT- TERM LIABILITIES", "TOTAL CURRENT LIABILITIES", "Total current liabilities","Short- term liabilities and short term provisions"," Short- term financial and operating liabilities"],
     "(AOP096)Short-term liabilities to affiliates": ["Short-term liabilities to affiliates","Liabilities of disposal groups", " Trade payables-foreing parent company, subsidiares and other associated companies", " Liabilities to affiliated companies","Group payables"],
@@ -1194,6 +1194,7 @@ if uploaded_file:
 
     except Exception as e:
         st.error(f"Error: {e}")
+
 
 
 
